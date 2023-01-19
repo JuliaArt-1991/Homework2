967175aa03b9125f27fc808c2ed87fc9c063cd58
Julia <artamon-julia@ya.ru>
2023-01-19 17:28:33 +0700

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите последовательность натуральных чисел, заканчивающуюся 0: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int n, max2 = Int32.MinValue;
+do {
+    n = Convert.ToInt32(Console.ReadLine()); 
+    if (n != 0) 
+{
+if (n > max)
+{
+    max2 = max;
+    max = n;
+}
+else if (n > max2)
+    max2 = n;
+}
+} while (n != 0);
+Console.WriteLine(max2);
